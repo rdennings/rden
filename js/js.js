bb4f7cc739633d752af30572914947832e1b3994
rdennings <rodney.dennings@gmail.com>
2015-02-16 19:26:08 -0500

    Revert "Revert "Website""
    This reverts commit 58b6604247b0e871d1b1dd2dc9ddf86018d870f2.

---
+++ js/js.js
@@ -0,0 +1,20 @@
+    function init() {
+        window.addEventListener('scroll', function(e){
+            var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+                shrinkOn = 20,
+                header = document.querySelector("header"),
+                c = "smaller";
+            if (distanceY > shrinkOn) {
+                header.classList.add( c );
+                logo.classList.add( c );
+                content.classList.add( c );
+            } else {
+                if (header.innerHTML != null){
+                    header.classList.remove( c );
+                    logo.classList.remove( c );
+                    content.classList.remove( c );
+                }
+            }
+        });
+    }
+    window.onload = init();
